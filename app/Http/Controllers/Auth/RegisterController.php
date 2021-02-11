045ba4fdf54c2b3af819132c3c2d3958f663ab2a
edvisyo <salminas@gmail.com>
2021-02-11 14:25:32 +0200

    changes in register controller

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -8,6 +8,8 @@ use App\Models\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use App\Models\Role;
+use Auth;
 
 class RegisterController extends Controller
 {
@@ -65,10 +67,17 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        $role = new Role();
+        $default_role_id = 2;
+
+        $user = User::create([    
             'birthdate' => $data['birthdate'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
+
+        $user->role()->attach($default_role_id);
+
+        return $user;
     }
 }
